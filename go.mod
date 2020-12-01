e925b74837b8f5c455b672aa48a8c4608597b1e3
Knappek <andreas.knapp-extern@deutschebahn.com>
2020-12-01 12:02:31 +0100

    create first working projects controller It does not do anything yet but only connects to gitlab
    Signed-off-by: Knappek <andreas.knapp-extern@deutschebahn.com>

--- go.mod
+++ go.mod
@@ -3,9 +3,15 @@ module github.com/crossplane-contrib/provider-gitlab
 go 1.13
 
 require (
+	cloud.google.com/go/logging v1.1.2
 	github.com/crossplane/crossplane-runtime v0.11.0
 	github.com/crossplane/crossplane-tools v0.0.0-20201026195708-a544f360b8ac
+	github.com/go-ini/ini v1.62.0
+	github.com/hashicorp/go-retryablehttp v0.6.4
+	github.com/pkg/errors v0.9.1
+	github.com/xanzy/go-gitlab v0.39.0
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+	k8s.io/api v0.18.6
 	k8s.io/apimachinery v0.18.6
 	sigs.k8s.io/controller-runtime v0.6.2
 	sigs.k8s.io/controller-tools v0.2.4
---
+++ pkg/clients/gitlab.go
@@ -0,0 +1,125 @@
+/*
+Copyright 2019 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package clients
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/crossplane/crossplane-runtime/pkg/resource"
+	"github.com/go-ini/ini"
+	"github.com/pkg/errors"
+	gitlab "github.com/xanzy/go-gitlab"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	runtimev1alpha1 "github.com/crossplane/crossplane-runtime/apis/core/v1alpha1"
+
+	"github.com/crossplane-contrib/provider-gitlab/apis/v1beta1"
+)
+
+// DefaultSection for INI files.
+const DefaultSection = ini.DefaultSection
+
+// Config provides gitlab configurations for the Gitlab client
+type Config struct {
+	Token   string
+	BaseURL string
+}
+
+// NewClient creates new Gitlab Client with provided Gitlab Configurations/Credentials.
+func NewClient(c Config) *gitlab.Client {
+	var cl *gitlab.Client
+	var err error
+	if c.BaseURL != "" {
+		cl, err = gitlab.NewClient(c.Token, gitlab.WithBaseURL(c.BaseURL))
+	} else {
+		cl, err = gitlab.NewClient(c.Token)
+	}
+	if err != nil {
+		panic(err)
+	}
+	return cl
+}
+
+// GetConfig constructs a Config that can be used to authenticate to Gitlab
+// API by the Gitlab Go client
+func GetConfig(ctx context.Context, c client.Client, mg resource.Managed) (*Config, error) {
+	switch {
+	case mg.GetProviderConfigReference() != nil:
+		return UseProviderConfig(ctx, c, mg)
+	default:
+		return nil, errors.New("providerConfigRef is not given")
+	}
+}
+
+// UseProviderConfig to produce a config that can be used to authenticate to AWS.
+func UseProviderConfig(ctx context.Context, c client.Client, mg resource.Managed) (*Config, error) {
+	// pc := &v1beta1.ProviderConfig{}
+	pc := &v1beta1.ProviderConfig{}
+	if err := c.Get(ctx, types.NamespacedName{Name: mg.GetProviderConfigReference().Name}, pc); err != nil {
+		return nil, errors.Wrap(err, "cannot get referenced Provider")
+	}
+
+	t := resource.NewProviderConfigUsageTracker(c, &v1beta1.ProviderConfigUsage{})
+	if err := t.Track(ctx, mg); err != nil {
+		return nil, errors.Wrap(err, "cannot track ProviderConfig usage")
+	}
+
+	switch s := pc.Spec.Credentials.Source; s { //nolint:exhaustive
+	case runtimev1alpha1.CredentialsSourceSecret:
+		csr := pc.Spec.Credentials.SecretRef
+		if csr == nil {
+			return nil, errors.New("no credentials secret referenced")
+		}
+		s := &corev1.Secret{}
+		if err := c.Get(ctx, types.NamespacedName{Namespace: csr.Namespace, Name: csr.Name}, s); err != nil {
+			return nil, errors.Wrap(err, "cannot get credentials secret")
+		}
+		return UseProviderSecret(ctx, s.Data[csr.Key], DefaultSection)
+	default:
+		return nil, errors.Errorf("credentials source %s is not currently supported", s)
+	}
+}
+
+// UseProviderSecret retrieves the gitlab token and base url that are being used
+// to authenticate to Gitlab.
+// Example:
+// [default]
+// token = <YOUR_PERSONAL_ACCESS_TOKEN>
+// base_url = <YOUR_GITLAB_BASE_URL>
+func UseProviderSecret(_ context.Context, data []byte, section string) (*Config, error) {
+	config, err := ini.InsensitiveLoad(data)
+	if err != nil {
+		return &Config{}, errors.Wrap(err, "cannot parse credentials secret")
+	}
+
+	iniSection, err := config.GetSection(section)
+	if err != nil {
+		return &Config{}, errors.Wrap(err, fmt.Sprintf("cannot get %s section in credentials secret", section))
+	}
+
+	token := iniSection.Key("token")
+	baseURL := iniSection.Key("base_url")
+
+	if token == nil || baseURL == nil {
+		return &Config{}, errors.New("returned key can be empty but cannot be nil")
+	}
+
+	return &Config{Token: token.Value(), BaseURL: baseURL.Value()}, nil
+}
