28495cca3a5dd433c6b1fe5a70dd293801816fed
Stanislav2017 <stanislavchichek@gmail.com>
2021-08-05 13:30:50 +0300

    Implement deploy token managed resource
    Signed-off-by: Stanislav2017 <stanislavchichek@gmail.com>

--- apis/projects/v1alpha1/zz_generated.managed.go
+++ apis/projects/v1alpha1/zz_generated.managed.go
@@ -20,6 +20,62 @@ package v1alpha1
 
 import xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
 
+// GetCondition of this DeployToken.
+func (mg *DeployToken) GetCondition(ct xpv1.ConditionType) xpv1.Condition {
+	return mg.Status.GetCondition(ct)
+}
+
+// GetDeletionPolicy of this DeployToken.
+func (mg *DeployToken) GetDeletionPolicy() xpv1.DeletionPolicy {
+	return mg.Spec.DeletionPolicy
+}
+
+// GetProviderConfigReference of this DeployToken.
+func (mg *DeployToken) GetProviderConfigReference() *xpv1.Reference {
+	return mg.Spec.ProviderConfigReference
+}
+
+/*
+GetProviderReference of this DeployToken.
+Deprecated: Use GetProviderConfigReference.
+*/
+func (mg *DeployToken) GetProviderReference() *xpv1.Reference {
+	return mg.Spec.ProviderReference
+}
+
+// GetWriteConnectionSecretToReference of this DeployToken.
+func (mg *DeployToken) GetWriteConnectionSecretToReference() *xpv1.SecretReference {
+	return mg.Spec.WriteConnectionSecretToReference
+}
+
+// SetConditions of this DeployToken.
+func (mg *DeployToken) SetConditions(c ...xpv1.Condition) {
+	mg.Status.SetConditions(c...)
+}
+
+// SetDeletionPolicy of this DeployToken.
+func (mg *DeployToken) SetDeletionPolicy(r xpv1.DeletionPolicy) {
+	mg.Spec.DeletionPolicy = r
+}
+
+// SetProviderConfigReference of this DeployToken.
+func (mg *DeployToken) SetProviderConfigReference(r *xpv1.Reference) {
+	mg.Spec.ProviderConfigReference = r
+}
+
+/*
+SetProviderReference of this DeployToken.
+Deprecated: Use SetProviderConfigReference.
+*/
+func (mg *DeployToken) SetProviderReference(r *xpv1.Reference) {
+	mg.Spec.ProviderReference = r
+}
+
+// SetWriteConnectionSecretToReference of this DeployToken.
+func (mg *DeployToken) SetWriteConnectionSecretToReference(r *xpv1.SecretReference) {
+	mg.Spec.WriteConnectionSecretToReference = r
+}
+
 // GetCondition of this Project.
 func (mg *Project) GetCondition(ct xpv1.ConditionType) xpv1.Condition {
 	return mg.Status.GetCondition(ct)
