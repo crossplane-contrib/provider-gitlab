25c5eb8dbb0a0ad7bfb0961878c3ad56defca1b7
Dario Zachow <dario.zachow@deutschebahn.com>
2024-08-05 08:20:42 +0200

    feat: bump gitlab dependency
    Signed-off-by: Dario Zachow <dario.zachow@deutschebahn.com>

--- pkg/clients/groups/group.go
+++ pkg/clients/groups/group.go
@@ -20,7 +20,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/xanzy/go-gitlab"
+	gitlab "github.com/xanzy/go-gitlab"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/crossplane-contrib/provider-gitlab/apis/groups/v1alpha1"
