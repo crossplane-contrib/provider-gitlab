ec79cfdb809de4cd9a8d1984f35a95780c3f3a24
BoxBoxJason <contact@boxboxjason.dev>
2025-12-02 23:15:47 +0100

    feat: add application settings controler
    Signed-off-by: BoxBoxJason <contact@boxboxjason.dev>

---
+++ pkg/namespaced/controller/instance/setup.go
@@ -0,0 +1,31 @@
+package instance
+
+import (
+	"github.com/crossplane-contrib/provider-gitlab/pkg/namespaced/controller/instance/settings"
+	"github.com/crossplane/crossplane-runtime/v2/pkg/controller"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+func Setup(mgr ctrl.Manager, o controller.Options) error {
+	for _, setup := range []func(ctrl.Manager, controller.Options) error{
+		settings.SetupApplicationSettings,
+	} {
+		if err := setup(mgr, o); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// SetupGated creates all Gitlab API controllers with the supplied logger and adds
+// them to the supplied manager with CRD gate support for SafeStart.
+func SetupGated(mgr ctrl.Manager, o controller.Options) error {
+	for _, setup := range []func(ctrl.Manager, controller.Options) error{
+		settings.SetupApplicationSettingsGated,
+	} {
+		if err := setup(mgr, o); err != nil {
+			return err
+		}
+	}
+	return nil
+}
