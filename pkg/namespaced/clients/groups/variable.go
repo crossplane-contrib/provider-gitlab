6f582f1d8d73c580bcd2d9de4d5ae02ef0015834
BoxBoxJason <contact@boxboxjason.dev>
2025-12-30 16:52:30 +0100

    docs: uniformize comment
    Signed-off-by: BoxBoxJason <contact@boxboxjason.dev>

--- pkg/namespaced/clients/groups/variable.go
+++ pkg/namespaced/clients/groups/variable.go
@@ -56,7 +56,7 @@ func GenerateVariableObservation(variable *gitlab.GroupVariable) v1alpha1.Variab
 	}
 }
 
-// LateInitializeVariable fills the empty fields in the groupVariable spec with the
+// LateInitializeVariable fills the empty fields in the Variable spec with the
 // values seen in gitlab.Variable.
 func LateInitializeVariable(in *v1alpha1.VariableParameters, variable *gitlab.GroupVariable) {
 	if variable == nil {
