cac804b753bd09a2e2d76cb00839e39e11021fc8
BoxBoxJason <contact@boxboxjason.dev>
2026-03-17 10:06:03 +0100

    feat: support project importURLSecretRef (#276)
    * feat: support project importURLSecretRef
    
    Signed-off-by: BoxBoxJason <contact@boxboxjason.dev>
    
    * fix: watch importURL changes
    
    Signed-off-by: BoxBoxJason <contact@boxboxjason.dev>
    
    * fix: up to date condition
    
    Signed-off-by: BoxBoxJason <contact@boxboxjason.dev>
    
    * fix: avoid updating CR with secret value
    
    Signed-off-by: BoxBoxJason <contact@boxboxjason.dev>
    
    * fix: generated code
    
    Signed-off-by: BoxBoxJason <contact@boxboxjason.dev>
    
    * fix: secret replacement in CR
    
    Signed-off-by: BoxBoxJason <contact@boxboxjason.dev>
    
    * fix: reconciliation loop
    
    Signed-off-by: BoxBoxJason <contact@boxboxjason.dev>
    
    * build: reset build submodule to match master
    
    Signed-off-by: BoxBoxJason <contact@boxboxjason.dev>
    
    ---------
    
    Signed-off-by: BoxBoxJason <contact@boxboxjason.dev>

---
+++ pkg/namespaced/controller/common/controller.go
@@ -0,0 +1,38 @@
+/*
+Copyright 2021 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package common
+
+import (
+	"context"
+
+	xpv1 "github.com/crossplane/crossplane-runtime/v2/apis/common/v1"
+	"github.com/crossplane/crossplane-runtime/v2/pkg/resource"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/crossplane-contrib/provider-gitlab/pkg/common"
+)
+
+// UpdateStringFromSecret fetches a sensitive setting value from a Kubernetes Secret
+// and assigns it to the provided parameter pointer.
+func UpdateStringFromSecret(mg resource.Managed, ctx context.Context, kube client.Client, selector *xpv1.LocalSecretKeySelector, param **string) error {
+	value, err := common.GetTokenValueFromLocalSecret(ctx, kube, mg, selector)
+	if err != nil {
+		return err
+	}
+	*param = value
+	return nil
+}
